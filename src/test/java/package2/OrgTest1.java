965b6876d9866e86a4da54fe445ace92a260d749
REKHA GUPTA <REKHA GUPTA@DESKTOP-8RNOGRT>
2024-05-31 12:17:04 +0530

    updation

--- src/test/java/package2/OrgTest1.java
+++ src/test/java/package2/OrgTest1.java
@@ -12,6 +12,10 @@ public class OrgTest1
 		String BROWSER= System.getProperty("browser");
 		String USERNAME= System.getProperty("username");
 		String PASSWORD= System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("Execute create organization test");
 	}
 
